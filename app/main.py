73ef2254b426cc307ee2f200003859f53b994c90
David Pessanha <davidppessanha@gmail.com>
2025-04-07 21:03:38 -0300

    created user route with me endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.routes import auth
+from app.routes import auth, user
 from app.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(user.router, prefix="/users", tags=["users"])
